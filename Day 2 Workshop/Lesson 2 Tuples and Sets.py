74e6a34b27a344aaf5e0e24e02f829f91ad13b36
Tcndiamond <106309388+Tcndiamond@users.noreply.github.com>
2025-04-17 13:07:51 +0200

    Updated and Deleted Files

--- Day 2 Workshop/Lesson 2 Tuples and Sets.py
+++ Day 2 Workshop/Lesson 2 Tuples and Sets.py
@@ -1,4 +1,14 @@
 my_tuple = ('apple', 'banana', 'cherry')
 my_list = list(my_tuple)
 
-print(my_list)
+my_list.append('orange')
+
+my_tuple = tuple(my_list)
+
+print(my_tuple)
+
+#Sets
+#Sets are unordered and unchangeable collections of data types
+#Duplicates are not allowed in sets
+set_fruits = {'apple', 'banana', 'cherry', 'orange', 'kiwi', 'orange'}
+print(set_fruits)
